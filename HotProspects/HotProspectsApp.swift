932c7c0238df536a9d81b7fd696de04f3497792f
Leo Barnett <leo.e.barnett@icloud.com>
2024-02-06 15:48:58 +0100

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Leo  on 06.02.24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
